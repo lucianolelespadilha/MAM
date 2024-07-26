ba64db16f5471b6e4b96328ef9cd082e92d9b65a
Luciano Leles Padilha <luciano.leles.padilha@gmail.com>
2024-07-25 23:59:16 -0300

    Creating the project

---
+++ src/main/java/com/llp/amm/AmmApplication.java
@@ -0,0 +1,13 @@
+package com.llp.amm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/llp/amm/AmmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llp.amm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
